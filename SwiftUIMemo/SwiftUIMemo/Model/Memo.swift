0cc7e53c76b501a4e2e9187fa6660ab5d258bb30
KangMinseok <qbq5000@gmail.com>
2020-05-10 14:50:08 +0900

    Add Model Class

---
+++ SwiftUIMemo/SwiftUIMemo/Model/Memo.swift
@@ -0,0 +1,27 @@
+//
+//  Memo.swift
+//  SwiftUIMemo
+//
+//  Created by 강민석 on 2020/05/10.
+//  Copyright © 2020 MinseokKang. All rights reserved.
+//
+
+import SwiftUI
+
+class Memo: Identifiable, ObservableObject {
+    let id: UUID
+    @Published var content: String
+    let insertDate: Date
+    
+    init(id: UUID = UUID(), content: String, insertDate: Date = Date()) {
+        self.id = id
+        self.content = content
+        self.insertDate = insertDate
+    }
+}
+
+extension Memo: Equatable {
+    static func == (lhs: Memo, rhs: Memo) -> Bool {
+        return lhs.id == rhs.id
+    }
+}
