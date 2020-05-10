80e1c90e054a2a8dfd1d7e4afd168c574b3c09c6
KangMinseok <qbq5000@gmail.com>
2020-05-10 13:41:28 +0900

    Initial Commit

---
+++ SwiftUIMemo/SwiftUIMemo/MemoListScene.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIMemo
+//
+//  Created by 강민석 on 2020/05/10.
+//  Copyright © 2020 MinseokKang. All rights reserved.
+//
+
+import SwiftUI
+
+struct MemoListScene: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct MemoListScene_Previews: PreviewProvider {
+    static var previews: some View {
+        MemoListScene()
+    }
+}
