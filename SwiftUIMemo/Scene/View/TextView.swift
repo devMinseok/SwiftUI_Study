b668afa095597fc171c1d36992e9863af2ebbfc4
KangMinseok <qbq5000@gmail.com>
2021-01-01 20:01:35 +0900

    ✨ 메모 작성 화면 TextView 추가

---
+++ SwiftUIMemo/Scene/View/TextView.swift
@@ -0,0 +1,41 @@
+//
+//  TextView.swift
+//  SwiftUIMemo
+//
+//  Created by 강민석 on 2021/01/01.
+//  Copyright © 2021 MinseokKang. All rights reserved.
+//
+
+import UIKit
+import SwiftUI
+
+struct TextView: UIViewRepresentable {
+    @Binding var text: String
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    func makeUIView(context: Context) -> UITextView {
+        let myTextView = UITextView()
+        myTextView.delegate = context.coordinator
+        
+        return myTextView
+    }
+    
+    func updateUIView(_ uiView: UITextView, context: Context) {
+        uiView.text = text
+    }
+    
+    class Coordinator: NSObject, UITextViewDelegate {
+        var parent: TextView
+        
+        init(_ uiTextView: TextView) {
+            self.parent = uiTextView
+        }
+        
+        func textViewDidChange(_ textView: UITextView) {
+            self.parent.text = textView.text
+        }
+    }
+}
