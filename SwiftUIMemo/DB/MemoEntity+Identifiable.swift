b6e08036441303f9896b00d6f50dfcb78a13a49f
KangMinseok <qbq5000@gmail.com>
2021-01-03 12:14:43 +0900

    ✨ CoreDataManager 추가

---
+++ SwiftUIMemo/DB/MemoEntity+Identifiable.swift
@@ -0,0 +1,13 @@
+//
+//  MemoEntity+Identifiable.swift
+//  SwiftUIMemo
+//
+//  Created by 강민석 on 2021/01/03.
+//  Copyright © 2021 MinseokKang. All rights reserved.
+//
+
+import SwiftUI
+
+extension MemoEntity {
+    
+}
