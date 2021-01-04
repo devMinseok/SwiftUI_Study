fd5c9f8f5318836ec6ee2e11bbad06e64fd94dc4
KangMinseok <qbq5000@gmail.com>
2021-01-04 17:47:35 +0900

    ✏️ Slider 드래그로 값을 선택하는 방법에 대해 공부함

--- Mastering-SwiftUI/MasteringSwiftUI/SystemViews/Slider.swift
+++ Mastering-SwiftUI/MasteringSwiftUI/SystemViews/Slider.swift
@@ -24,28 +24,83 @@
 import SwiftUI
 
 struct View_Slider: View {
-   @State private var r = 0.0
-   @State private var g = 0.0
-   @State private var b = 0.0
-   
-   var color: Color {
-      Color(red: r / 255, green: g / 255, blue: b / 255)
-   }
-   
-   var body: some View {
-      VStack {
-         Image(systemName: "smiley.fill")
-            .resizable()
-            .foregroundColor(color)
-            .frame(width: 250, height: 250)
-         
-         
-      }
-   }
+    @State private var r = 0.0
+    @State private var g = 0.0
+    @State private var b = 0.0
+    
+    var color: Color {
+        Color(red: r / 255, green: g / 255, blue: b / 255)
+    }
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "smiley.fill")
+                .resizable()
+                .foregroundColor(color)
+                .frame(width: 250, height: 250)
+            
+            HStack {
+                Text("R")
+                Slider(value: $r,
+                       in: 0...255,
+                       step: 1,
+                       label: {
+                        EmptyView()
+                       })
+                
+                HStack {
+                    Spacer()
+                    
+                    Text("\(Int(r))")
+                }
+                .frame(width: 40)
+            }
+            .padding()
+            .foregroundColor(.red)
+            
+            HStack {
+                Text("G")
+                Slider(value: $g,
+                       in: 0...255,
+                       step: 1,
+                       label: {
+                        EmptyView()
+                       })
+                
+                HStack {
+                    Spacer()
+                    
+                    Text("\(Int(g))")
+                }
+                .frame(width: 40)
+            }
+            .padding()
+            .foregroundColor(.red)
+            
+            HStack {
+                Text("B")
+                Slider(value: $b,
+                       in: 0...255,
+                       step: 1,
+                       label: {
+                        EmptyView()
+                       })
+                
+                HStack {
+                    Spacer()
+                    
+                    Text("\(Int(b))")
+                }
+                .frame(width: 40)
+            }
+            .padding()
+            .foregroundColor(.red)
+        }
+    }
 }
 
 struct View_Slider_Previews: PreviewProvider {
-   static var previews: some View {
-      View_Slider()
-   }
+    static var previews: some View {
+        View_Slider()
+    }
 }
