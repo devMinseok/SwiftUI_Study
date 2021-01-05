8d26e40c5221365718af78d78a11986c538c0bfc
KangMinseok <qbq5000@gmail.com>
2021-01-05 14:00:07 +0900

    ✏️ Text - Localized String - Standard Font Styles - font modifier - foregroundColor와 background - lineLimit - truncationMode - multilineTextAlignment - lineSpacing - allowsTightening - minimumScaleFactor
    화면에 문자열을 출력하는 방법을 공부함

--- Mastering-SwiftUI/MasteringSwiftUI/TextAndFont/Text.swift
+++ Mastering-SwiftUI/MasteringSwiftUI/TextAndFont/Text.swift
@@ -26,15 +26,41 @@ import SwiftUI
 let longText = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
 struct View_Text: View {
-   var body: some View {
-      VStack {
-         Text("Hello, World!")            
-      }
-   }
+    var body: some View {
+        VStack {
+            // 자동으로 LocalizedStringKey를 검색하여 매칭
+            //Text("Hello, World!")
+            
+            // 수동으로LocalizedStringKey를 입력
+            //Text(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/, tableName: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Table Name@*/nil/*@END_MENU_TOKEN@*/, bundle: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Bundle@*/nil/*@END_MENU_TOKEN@*/, comment: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Comment@*/nil/*@END_MENU_TOKEN@*/)
+            
+            // LocalizedStringKey를 찾지 않음
+            //Text(verbatim: /*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/)
+            
+            Text("Hello, World!")
+                .font(.largeTitle)
+                .foregroundColor(.white)
+                .background(Color.black)
+            
+            Text(longText)
+                .frame(width: 300)
+                .lineLimit(7)
+                .truncationMode(.tail) // 생략할 위치 (...)
+                .multilineTextAlignment(.center) // text 정렬
+                .lineSpacing(5) // 열간 거리
+            
+            Text("Lorem ipsum dolor sit amet, consectetur")
+                .font(.largeTitle)
+                .frame(width: 300)
+                .lineLimit(1) // 열 제한
+                .allowsTightening(true) // 자간
+                .minimumScaleFactor(0.5) // 사이즈
+        }
+    }
 }
 
 struct View_Text_Previews: PreviewProvider {
-   static var previews: some View {
-      View_Text()
-   }
+    static var previews: some View {
+        View_Text()
+    }
 }
