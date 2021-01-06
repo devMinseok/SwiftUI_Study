031c46dc8520c0d0ae78355c8bdfe010c567fa84
KangMinseok <qbq5000@gmail.com>
2021-01-06 18:19:47 +0900

    ✏️ Color - Color 구조체 - 시스템 컬러 - UIColor를 Color로 변환 - Color Asseet과 Named Color
    컬러를 다루는 다양한 방법을 공부함

--- Mastering-SwiftUI/MasteringSwiftUI/Color/ColorBasics.swift
+++ Mastering-SwiftUI/MasteringSwiftUI/Color/ColorBasics.swift
@@ -24,28 +24,34 @@
 import SwiftUI
 
 struct ColorBasics: View {
-   
-   var body: some View {
-      VStack {
-         Text("Hello, SwiftUI Color!")
-            .font(.largeTitle)            
-         
-         //
-      }
-      .background(Color(UIColor.systemBackground))
-      
-   }
+    
+    var body: some View {
+        VStack {
+            Text("Hello, SwiftUI Color!")
+                .font(.largeTitle)
+            
+            Color.blue
+            
+            Color.yellow
+            
+            Color(.sRGB, red: 70.0/255.0, green: 53.0/255.0, blue: 99.0/255.0, opacity: 1.0)
+            
+            // Named Color (오타 조심할 것)
+            Color("myCustomColor")
+        }
+        .background(Color(UIColor.systemBackground))
+    }
 }
 
 struct ColorBasics_Previews: PreviewProvider {
-   static var previews: some View {
-      Group {
-         ColorBasics()
-            .environment(\.colorScheme, .light)
-         
-         ColorBasics()
-            .environment(\.colorScheme, .dark)
-      }
-      
-   }
+    static var previews: some View {
+        Group {
+            ColorBasics()
+                .environment(\.colorScheme, .light)
+            
+            ColorBasics()
+                .environment(\.colorScheme, .dark)
+        }
+        
+    }
 }
