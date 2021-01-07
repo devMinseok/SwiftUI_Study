d40bed22fa98192e984db9ccd37acbfe3106b64e
KangMinseok <qbq5000@gmail.com>
2021-01-07 15:54:55 +0900

    ✏️ Animation Basics - 애니메이션 구현 기초 - animationmodifier - 애니메이션 타이밍 - 실행 시간과 지연 시간 지정 - Explicit Animation
    가장 기본적인 애니메이션을 구현

--- Mastering-SwiftUI/MasteringSwiftUI/Animation/AnimationBasics.swift
+++ Mastering-SwiftUI/MasteringSwiftUI/Animation/AnimationBasics.swift
@@ -24,30 +24,32 @@
 import SwiftUI
 
 struct AnimationBasics: View {
-   @State private var position = CGPoint.zero
-   
-   var body: some View {
-      VStack {
-         Circle()
-            .foregroundColor(.blue)
-            .frame(width: 50, height: 50)
-            .position(position)
-            .offset(x: 50, y: 50)
+    @State private var position = CGPoint.zero
+    
+    var body: some View {
+        VStack {
+            Circle()
+                .foregroundColor(.blue)
+                .frame(width: 50, height: 50)
+                .position(position)
+                .offset(x: 50, y: 50)
+                //.animation(Animation.easeInOut(duration: 3).delay(1))
+                .animation(Animation.easeInOut(duration: 3).speed(2))
             
-         Spacer()
-         
-         Button(action: {
-            self.position = self.position == .zero ? CGPoint(x: 300, y: 500) : .zero
-         }, label: {
-            Text("Animate")
-         })
-         .padding()
-      }
-   }
+            Spacer()
+            
+            Button(action: {
+                self.position = self.position == .zero ? CGPoint(x: 300, y: 500) : .zero
+            }, label: {
+                Text("Animate")
+            })
+            .padding()
+        }
+    }
 }
 
 struct AnimationBasics_Previews: PreviewProvider {
-   static var previews: some View {
-      AnimationBasics()
-   }
+    static var previews: some View {
+        AnimationBasics()
+    }
 }
