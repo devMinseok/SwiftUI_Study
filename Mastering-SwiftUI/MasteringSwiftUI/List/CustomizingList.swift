cfad6b146e8cf2a8a0bf3302cac040b5966f8d86
KangMinseok <qbq5000@gmail.com>
2021-01-08 12:42:41 +0900

    ✏️ Customizing List - List Styles - listRowInsets - listRowBackground - Section Header/Footer View
    리스트를 다양한 형태로 커스터마이징 하는 방법을 공부함

--- Mastering-SwiftUI/MasteringSwiftUI/List/CustomizingList.swift
+++ Mastering-SwiftUI/MasteringSwiftUI/List/CustomizingList.swift
@@ -23,31 +23,66 @@
 
 import SwiftUI
 
+struct HeaderView: View {
+    let title: String
+    let imageName: String
+    
+    var body: some View {
+        HStack {
+            Text(title)
+            Image(systemName: imageName)
+        }
+        .font(.title)
+    }
+}
+
+struct FooterView: View {
+    let title: String
+    let imageName: String
+    
+    var body: some View {
+        VStack {
+            Text(title)
+            Image(systemName: imageName)
+        }
+        .font(.title)
+    }
+}
+
 struct CustomizingList: View {
-   var body: some View {
-      VStack {
-         Text("Customizing List")
-            .font(.largeTitle)
-            .listRowBackground(Color.red)
-
-         List {
-            Section() {
-               Text("Hello, List!")
-               Text("List Row Insets")
-               Text("List Row Background")
+    var body: some View {
+        VStack {
+            Text("Customizing List")
+                .font(.largeTitle)
+                .listRowBackground(Color.red)
+            
+            List {
+                Section(header: HeaderView(title: "Header", imageName: "star"),
+                        footer: FooterView(title: "Footer", imageName: "star")) {
+                    Text("Hello, List!")
+                    // 가장 근접한 Modifier가 우선순위가 높음
+                    Text("List Row Insets")
+                        .listRowInsets(.init(top: 0, leading: 100, bottom: 0, trailing: 0))
+                    // 가장 근접한 Modifier가 우선순위가 높음
+                    Text("List Row Background")
+                        .listRowBackground(Color.blue)
+                }
+                .listRowInsets(.init(top: 0, leading: 60, bottom: 0, trailing: 0))
+                .listRowBackground(Color.yellow)
+                
+                Section() {
+                    Text("One")
+                    Text("Two")
+                }
+                .listRowInsets(.init(top: 0, leading: 100, bottom: 0, trailing: 0))
             }
-
-            Section() {
-               Text("One")
-               Text("Two")
-            }
-         }
-      }
-   }
+            .listStyle(GroupedListStyle())
+        }
+    }
 }
 
 struct CustomizingList_Previews: PreviewProvider {
-   static var previews: some View {
-      CustomizingList()
-   }
+    static var previews: some View {
+        CustomizingList()
+    }
 }
